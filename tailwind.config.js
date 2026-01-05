b49134065ae97cd27bc27e857d5c15fd2b6852cc
Gagan Bhardwaj <gaganbhardwaj514@gmail.com>
2026-01-05 23:33:31 +0530

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,14 +55,6 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        success: {
-          DEFAULT: "hsl(var(--success))",
-          foreground: "hsl(var(--success-foreground))",
-        },
-        warning: {
-          DEFAULT: "hsl(var(--warning))",
-          foreground: "hsl(var(--warning-foreground))",
-        },
         sidebar: {
           DEFAULT: "hsl(var(--sidebar))",
           foreground: "hsl(var(--sidebar-foreground))",
